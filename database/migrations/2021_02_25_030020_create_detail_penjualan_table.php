44dd08d70b03b9bb9dac29c7b4044729601ae3c5
Rizqi Ghozali <m.rizqi.ghozali@gmail.com>
2021-03-17 10:57:38 +0700

    adding migration

---
+++ database/migrations/2021_02_25_030020_create_detail_penjualan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailPenjualanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_penjualan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('penjualan_id');
+            $table->foreignId('barang_id');
+            $table->unsignedBigInteger('harga_jual');
+            $table->unsignedSmallInteger('jumlah');
+            $table->unsignedBigInteger('sub_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_penjualan');
+    }
+}
