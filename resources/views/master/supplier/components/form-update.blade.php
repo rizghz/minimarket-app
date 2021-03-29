b2b981e66b505a1563779816e586a106c5ee98f8
Rizqi Ghozali <m.rizqi.ghozali@gmail.com>
2021-03-29 14:32:43 +0700

    CRUD supplier

---
+++ resources/views/master/supplier/components/form-update.blade.php
@@ -0,0 +1,135 @@
+<!-- form -->
+<form class="form-horizontal" 
+      action="{{ route('supplier.index') }}"
+      method="post"
+      id="form-supplier-update">
+
+  @method('patch')
+  @csrf
+
+  <!-- form kode supplier -->
+  <div class="form-group row">
+    <label for="kode_supplier" class="col-sm-3 col-form-label text-md-right text-white">Kode Supplier</label>
+    <div class="col-sm-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text text-muted enigma-dark-bg-2 border-0">
+            <i class="fa fa-pencil pl-1"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control input text-white border-0" 
+               id="kode_supplier"
+               name="kode_supplier" 
+               style="background-color: #3B4252" 
+               placeholder="kode supplier" 
+               autocomplete="off">
+      </div>
+      <div id="invalid-feedback-kode_supplier" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form kode supplier -->
+
+  <!-- form nama supplier -->
+  <div class="form-group row">
+    <label for="nama_supplier" class="col-sm-3 col-form-label text-md-right text-white">Nama Supplier</label>
+    <div class="col-sm-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text text-muted enigma-dark-bg-2 border-0">
+            <i class="fa fa-pencil pl-1"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control input text-white border-0" 
+              id="nama_supplier"
+              name="nama_supplier" 
+              style="background-color: #3B4252"
+              placeholder="nama supplier" 
+              autocomplete="off">
+      </div>
+      <div id="invalid-feedback-nama_supplier" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form nama supplier -->
+
+  <!-- form alamat -->
+  <div class="form-group row">
+    <label for="alamat" class="col-sm-3 col-form-label text-md-right text-white">Alamat</label>
+    <div class="col-sm-8">
+      <textarea class="form-control input text-white border-0" 
+                rows="3"
+                id="alamat" 
+                name="alamat" 
+                style="background-color: #3B4252" 
+                placeholder="input alamat" 
+                autocomplete="off"></textarea>
+      <div id="invalid-feedback-alamat" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form alamat -->
+
+  <!-- form kota -->
+  <div class="form-group row">
+    <label for="kota" class="col-sm-3 col-form-label text-md-right text-white">Kota</label>
+    <div class="col-sm-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text text-muted enigma-dark-bg-2 border-0">
+            <i class="fa fa-pencil pl-1"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control input text-white border-0" 
+              id="kota"
+              name="kota" 
+              style="background-color: #3B4252"
+              placeholder="kota" 
+              autocomplete="off">
+      </div>
+      <div id="invalid-feedback-kota" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form kota -->
+
+  <!-- form nomor telepon -->
+  <div class="form-group row">
+    <label for="no_telp" class="col-sm-3 col-form-label text-md-right text-white">Nomor Telepon</label>
+    <div class="col-sm-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text text-muted enigma-dark-bg-2 border-0">
+            <i class="fa fa-pencil pl-1"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control input text-white border-0" 
+               id="no_telp" 
+               name="no_telp" 
+               style="background-color: #3B4252" 
+               placeholder="input nomor telepon" 
+               autocomplete="off">
+      </div>
+      <div id="invalid-feedback-no_telp" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form nomor telepon -->
+
+  <!-- tombol -->
+  <div class="form-group row mt-3 mb-0">
+    <div class="col-6 offset-3">
+
+      <!-- tombol submit -->
+      <button type="button" class="btn enigma-dark-bg-2 text-white rounded mr-2" id="btn-submit" data-form="#form-supplier-update">
+        <i class="fa fa-save mx-1"></i> Submit
+      </button>
+      <!-- akhir tombol submit -->
+
+      <!-- tombol batal -->
+      <button type="button" class="btn btn-danger rounded" data-dismiss="modal">
+        <i class="fa fa-chevron-left mx-1"></i> Batal
+      </button>
+      <!-- akhir tombol batal -->
+
+    </div>
+  </div>
+  <!-- akhir tombol -->
+
+</form>
+<!-- akhir form -->
--- resources/views/master/supplier/index.blade.php
+++ resources/views/master/supplier/index.blade.php
@@ -4,9 +4,17 @@
 
 @section('content')
 
+  <button class="btn btn-lg enigma-dark-bg-1 text-white rounded mb-4 tambah-trigger"
+          data-toggle="modal"
+          data-target="#form-modal-tambah">
+    <i class="fa fa-plus mx-1"></i>
+    <span class="d-none d-md-inline">Tambah</span>
+  </button>
+
   <div class="row">
     <div class="col-md-12">
       <div class="enigma-dark-bg-1 shadow bgc-white bd bdrs-3 p-20 mB-20">
+        @include('master.supplier.components.dialog')
         @include('master.supplier.components.table')
       </div>
     </div>
@@ -27,43 +35,71 @@
     }).always(function(a, b, c) { console.log(c); });
     return buffer;
   };
-  
-  let asyncReload = (route) => {
-    $.get({ 
-        url: route, 
-        success: function(result) {
-            $('.main-content').html($(result).find('.mainContent'));
-        }
+
+  $(() => {
+    let formTambah = $('#form-supplier-tambah').find('.form-control.input');
+    let formUpdate = $('#form-supplier-update').find('.form-control.input');
+    let dataId = "";
+    $('body').on('click', '.tambah-trigger', function(e) {
+      e.preventDefault();
+      for (let i = 0; i < formTambah.length; i++) {
+        formTambah.eq(i).val(null).trigger('change');
+      }
     });
-  };
+    $('body').on('click', '.update-trigger', function(e) {
+      e.preventDefault();
+      let data = $(this).data();
+      dataId = data['id'];
+      for (let i = 0; i < formUpdate.length; i++) {
+        formUpdate.eq(i).val(data[formUpdate.eq(i).attr('name')]).trigger('change');
+      }
+    });
+    $('body').on('click', '#btn-submit', function(e) {
+      e.preventDefault();
+      let form = $($(this).data('form'));
+      let res = request(`${form.attr('action')}/${dataId}`, 'post', form.serialize());
+      if (res['status'] == 200) {
+        result = parseInt(res['responseText']);
+        if (!result) {
+          alert('failed');
+          location.reload();
+        }
+        alert('sucess');
+        location.reload();
+      } else {
+        let error = res['responseJSON']['errors'];
+        for (let buffer in error) {
+          $('#invalid-feedback-' + buffer).text(`${error[buffer]}`);
+        }
+      }
+    });
+  });
 
   let table = $('table').DataTable({
-        "info": false, 
-        "paging": true,
-        "lengthChange": false,
-        "pageLength": 13,
-        "dom": 'lrtip', 
-        "language": {
-            "emptyTable": "<div class='py-1'>Tidak ada data di dalam tabel</div>",
-            "paginate": {
-                "previous": "<i class='ti-angle-left'></i>",
-                "next": "<i class='ti-angle-right'></i>"
-            }
-        },
-        "createdRow": function(r) {
-            $(r).children().addClass('py-2 border-0');
-        },
-        "preDrawCallback": function(s) {
-            $('table tbody').hide();
-        },
-        "drawCallback": function() {
-            $('ul.pagination').addClass('justify-content-center my-2');
-            $('a.page-link').addClass('mx-1');
-            $('table tbody').fadeIn(700);
+    "info": false,
+    "paging": true,
+    "lengthChange": false,
+    "pageLength": 11,
+    "dom": 'lrtip',
+    "language": {
+        "emptyTable": "<div class='py-1 text-center'>Tidak ada data di dalam tabel</div>",
+        "paginate": {
+            "previous": "<i class='ti-angle-left'></i>",
+            "next": "<i class='ti-angle-right'></i>"
         }
-    });
-
-    $('td.dataTables_empty').addClass('py-2 border-0');
+    },
+    "createdRow": function(r) {
+        $(r).children().addClass('py-2 border-0');
+    },
+    "preDrawCallback": function(s) {
+        $('table tbody').hide();
+    },
+    "drawCallback": function() {
+        $('ul.pagination').addClass('justify-content-center my-2');
+        $('a.page-link').addClass('mx-1');
+        $('table tbody').fadeIn(700);
+    }
+  });
 
 </script>
 @endpush
