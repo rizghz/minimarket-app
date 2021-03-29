4930039ba9026180f6e986639a88840246dc3021
Rizqi Ghozali <m.rizqi.ghozali@gmail.com>
2021-03-29 23:59:23 +0700

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\Barang;
+use App\Models\Customer;
 use App\Models\Produk;
 use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+    
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        Produk::factory(10)->create();
-        Supplier::factory(10)->create();
-        Barang::factory(10)->create();
+
+    public function run() {
+        User::factory(10)->create();
+        Produk::factory(20)->create();
+        Barang::factory(20)->create();
+        Supplier::factory(20)->create();
+        Customer::factory(20)->create();
     }
+
 }
