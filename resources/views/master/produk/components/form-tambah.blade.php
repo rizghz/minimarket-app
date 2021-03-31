0205c85b62642694bbdc6149ddce1882e193fa44
Rizqi Ghozali <m.rizqi.ghozali@gmail.com>
2021-03-31 09:52:40 +0700

    CRUD produk

---
+++ resources/views/master/produk/components/form-tambah.blade.php
@@ -0,0 +1,53 @@
+<!-- form -->
+<form class="form-horizontal" 
+      action="{{ route('produk.store') }}"
+      method="post"
+      id="form-produk-tambah">
+
+  @method('post')
+  @csrf
+
+  <!-- form nama produk -->
+  <div class="form-group row">
+    <label for="nama" class="col-sm-3 col-form-label text-md-right text-white">Nama Produk</label>
+    <div class="col-sm-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text text-muted enigma-dark-bg-2 border-0">
+            <i class="fa fa-pencil pl-1"></i>
+          </span>
+        </div>
+        <input type="text" class="form-control input text-white border-0" 
+              id="nama"
+              name="nama" 
+              style="background-color: #3B4252"
+              placeholder="nama" 
+              autocomplete="off">
+      </div>
+      <div id="invalid-feedback-nama" class="invalid alert-text text-sm pt-2"></div>
+    </div>
+  </div>
+  <!-- akhir form nama produk -->
+
+  <!-- tombol -->
+  <div class="form-group row mt-3 mb-0">
+    <div class="col-6 offset-3">
+
+      <!-- tombol submit -->
+      <button type="button" class="btn enigma-dark-bg-2 text-white rounded mr-2" id="btn-submit" data-form="#form-produk-tambah">
+        <i class="fa fa-save mx-1"></i> Submit
+      </button>
+      <!-- akhir tombol submit -->
+
+      <!-- tombol batal -->
+      <button type="button" class="btn btn-danger rounded" data-dismiss="modal">
+        <i class="fa fa-chevron-left mx-1"></i> Batal
+      </button>
+      <!-- akhir tombol batal -->
+
+    </div>
+  </div>
+  <!-- akhir tombol -->
+
+</form>
+<!-- akhir form -->
---
+++ resources/views/master/produk/components/table.blade.php
@@ -0,0 +1,50 @@
+<h4 class="text-white mB-20">Data Produk</h4>
+
+<table class="table table-striped enigma-dark-bg-1 text-white border-0">
+
+  <thead>
+    <tr>
+      <th data-orderable="false" class="pl-4 border-0">No</th>
+      <th data-orderable="false" class="border-0">Nama</th>
+      <th data-orderable="false" class="border-0"></th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    
+    @foreach ($data as $item)
+      
+      <tr>
+        
+        <td class="py-2 border-0">
+          <div class="py-1 pl-3">{{ $loop->iteration }}</div>
+        </td>
+        
+        <td class="py-2 border-0">
+          <div class="py-1">{{ $item->nama }}</div>
+        </td>
+        
+        <td class="py-2 border-0">
+          
+          <button class="btn enigma-dark-bg-3 btn-sm my-0 mx-0 mr-1 px-2 py-1 rounded update-trigger"
+             data-id="{{ $item->id }}"
+             data-nama="{{ $item->nama }}"
+             data-toggle="modal"
+             data-target="#form-modal-update">
+            <i class="fa fa-pencil text-white"></i>
+          </button>
+          
+          <button class="btn btn-danger btn-sm my-0 mx-1 px-2 py-1 rounded delete-trigger"
+             data-id="{{ $item->id }}">
+            <i class="fa fa-trash"></i>
+          </button>
+
+        </td>
+
+      </tr>
+
+    @endforeach
+
+  </tbody>
+
+</table>
