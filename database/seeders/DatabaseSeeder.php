a0d9486dbbc05f3c0f09dbf5052d3f051c6b0a3c
Rizqi Ghozali <m.rizqi.ghozali@gmail.com>
2021-04-03 19:56:54 +0700

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Barang;
 use App\Models\Customer;
-use App\Models\Produk;
 use App\Models\Supplier;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
@@ -18,11 +15,8 @@ class DatabaseSeeder extends Seeder {
      */
 
     public function run() {
-        User::factory(10)->create();
-        Produk::factory(20)->create();
-        Barang::factory(20)->create();
-        Supplier::factory(20)->create();
-        Customer::factory(20)->create();
+        Supplier::factory(30)->create();
+        Customer::factory(30)->create();
     }
 
 }
